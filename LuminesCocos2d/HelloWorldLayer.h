895251415a8c874a754970ea91d43825dd1bdc31
Wang Lei <wanglei@Wang-Leis-Mac-mini.local>
2011-08-31 21:04:01 +0800

    Initial Commit

---
+++ LuminesCocos2d/HelloWorldLayer.h
@@ -0,0 +1,21 @@
+//
+//  HelloWorldLayer.h
+//  LuminesCocos2d
+//
+//  Created by Lei Wang on 31/8/11.
+//  Copyright Virtuos 2011. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
